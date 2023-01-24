90f824dcbab2d5ab21dedb62e046893a4a117206
Dohyeon An <dev.dohyeonan@gmail.com>
2023-01-24 18:06:55 +0900

    refactor: react 버전 변경으로 인한 설정 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
@@ -9,9 +9,11 @@ function App() {
   return (
     <HashRouter>
       <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
     </HashRouter>
   );
 }
